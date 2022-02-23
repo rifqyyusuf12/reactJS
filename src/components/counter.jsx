e2d8099830b45508347b42ba9c32f881543410f2
rifqyyusuf12 <rifqy@importir.co>
2022-02-23 19:22:09 +0700

    latihan react

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state ={ 
+        count : 0
+    }; 
+    render() { 
+        return (
+        <div>
+            <span>{this.formatCount()}</span>
+            <button>Increment</button>
+        </div>
+        );
+    }
+
+    formatCount() {
+        const {count} = this.state
+        const z = <h1>Zero</h1>
+        return count === 0 ? z : count;
+    }
+}
+ 
+export default Counter;
